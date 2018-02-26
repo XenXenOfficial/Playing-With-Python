edbb5f8bb7b9b295e4591d2eb9136bb1a07900d0
XenXenOfficial <9jaydon99@gmail.com>
2018-02-26 06:56:03 -0500

    Update SaveFilePractice-FileManipulation.py
    Changed names around and made it look more proper according to the PEP8

--- SaveFilePractice-FileManipulation.py
+++ SaveFilePractice-FileManipulation.py
@@ -1,94 +1,97 @@
-import os, glob
+import os
+import glob
 '''
 Anything with
 try:
 except:
 is pythons way of catching errors. The following catches in the program
-is to make sure that the file actually exists. Except one special case on line 133
+is to make sure that the file actually exists.
 '''
 
 
-dLetter = os.getcwd() #Grabs the current directory
+d_Letter = os.getcwd() #Grabs the current directory
 
-def fileNoExtension(): #Function to get files without a file extension
+def file_No_Extension(): #Function to get files without a file extension
     #Borrowed from Keatinge
     files_no_ext = ["\n".join(f.split(".")[:-1]) for f in os.listdir() if os.path.isfile(f)] #Puts it all on a new line, 
                                                                                              #removes the periods, deletes the extensions 
                                                                                              #for each file printed IF its a file.
     return "\n".join(files_no_ext) #Returns the files in a pretty list
 
-def changeDir(change): #Change the directory and other things
+def change_Dir(change): #Change the directory and other things
     check = 'y'
-    fileDir = change
+    file_Dir = change
+    print(file_Dir)
     while check != 'n':
-        print(fileDir)
         print("\nChange directory? (y/n/Type add with a following name to add onto current directory/ ~ to go back)")
         print("Ex. 'add *Folder name*', or type 'List' to view the folders ")
-        uInput = input(">  ")
-        if 'List' in uInput:
-            folders = [f for f in os.listdir() if os.path.isdir(f)]
+        u_Input = input(">  ")
+        if 'List' in u_Input:
+            folders = [f for f in os.listdir() if os.path.isdir(f)] #Makes f equal all the current directory folder names
             for i in folders:
-                print(i)
+                print("\n" + i)
         else:            
-            addCheck = []
-            addCheck = uInput.split(" ")
-            if 'add' not in addCheck:
-                if '~' not in addCheck:
-                    if uInput not in ('y', 'n'):
+            add_Check = []
+            add_Check = u_Input.split(" ")
+            if 'add' not in add_Check:
+                if '~' not in add_Check:
+                    if u_Input not in ('y', 'n'):
                         print("\nPlease put a valid answer")
                         continue
                     else:
-                        if uInput in 'y':
+                        if u_Input in 'y':
                             quitLoop = ''
                             while quitLoop != 'q':
-                                tempVal = fileDir
-                                fileDir = input("\nWhat do you want it changed to? (q to quit):  ")
-                                if fileDir not in ("\\", 'q'):
-                                    fileDir += "\\" #Adds on the input value
+                                temp_Val = file_Dir
+                                file_Dir = input("\nWhat do you want it changed to? (q to quit):  ")
+                                if file_Dir not in ("\\", 'q'):
+                                    file_Dir += "\\" #Adds on the input value
                                     try:
-                                        os.chdir(fileDir)
-                                        uInput = 'n'
+                                        os.chdir(file_Dir)
+                                        u_Input = 'n'
                                         quitLoop = 'q'
                                     except FileNotFoundError:
                                         print("That directory doesn't exist!")
                                         continue
-                                if fileDir in 'q': #Checks if there is a quit
+                                if file_Dir in 'q': #Checks if there is a quit
                                     continue
-                    if 'n' in addCheck: #Checks if a n was sent instead
-                        return fileDir 
-                if '~' in addCheck:             
-                    fileDir = os.path.dirname(fileDir) 
-            elif 'add' in addCheck:
+                    if 'n' in add_Check: #Checks if a n was sent instead
+                        return file_Dir 
+                if '~' in add_Check:             
+                    file_Dir = os.path.dirname(file_Dir)
+                    os.chdir(file_Dir)
+                    print("\n" + file_Dir)
+            elif 'add' in add_Check:
                 try:
-                    tempFileDir = fileDir
-                    fileDir = fileDir + "\\" + addCheck[1]
-                    os.chdir(fileDir)
+                    temp_File_Dir = file_Dir
+                    file_Dir = file_Dir + "\\" + add_Check[1]
+                    os.chdir(file_Dir)
+                    print("\n" + file_Dir)
                 except FileNotFoundError:
                     print("That directory doesn't exist!")
-                    fileDir = tempFileDir #Gets changed back to the temp value
+                    file_Dir = temp_File_Dir #Gets changed back to the temp value
                     continue
 
-def fileFinder(chosenFile): #Finds a file without a extension
+def file_Finder(chosen_File): #Finds a file without a extension
     #Borrowed from Samuele Mattiuzzo
-    
     #Glob finds all the pathnames matching a specified pattern. 
-    path = fileDir
-    for infile in glob.glob(os.path.join(path, chosenFile + '.*')): #The pattern? The name of the file is the pattern
+    path = file_Dir
+    for infile in glob.glob(os.path.join(path, chosen_File + '.*')): #The pattern? The name of the file is the pattern
                                                                     #and it grabs whatever extension it is.
         return infile #Returns the file extension
 
-def fileSizeFinder(size): #Finds the size of a file 
+def file_Size_Finder(size): #Finds the size of a file 
     #Thanks Blue for teaching me Enumerate and other things
-    byteList = [" B", " MB", " KB", " GB", " TB", " PB"]
-    for sVal in byteList:
-        if round(size, 3) > 0:
-            print(round(size, 3), SVal)
-            size /= 1024
+    byte_List = [" B", " MB", " KB", " GB", " TB", " PB"]
+    for sVal in byte_List: #Makes SVal go through the list above
+        if round(size, 3) > 0: #Checks if the size is greater than 0
+            print(round(size, 3), SVal) #Prints it rounded
+            size /= 1024 #Divides it by 1024 to get the next file size (If B it'll go to MB
         
-fileDir = changeDir(dLetter)
+file_Dir = change_Dir(d_Letter)
 
 
-optionList = ["1. List options", 
+option_List = ["1. List options", 
               " 2. List files with extension", 
               " 3. Create a folder", 
               " 4. Get chosen file size",
@@ -99,34 +102,34 @@ optionList = ["1. List options",
               " 9. Current Directory",
               " ` Advanced creation (Access to Command Prompt)" ]
 
-print("\n What do you want to do? ", '\n\n' , '\n\n'.join(optionList))
+print("\n What do you want to do? ", '\n\n' , '\n\n'.join(option_List))
 
 
 while True:
     option = input("> ")    
    
     if option == '1':
-        print('\n'.join(optionList)) #Prints the options
+        print('\n'.join(option_List)) #Prints the options
     
     elif option == '2':
         print('\n'.join(os.listdir())) #Prints the dir folders and files
    
     elif option == '3':
-        fName = input("\nFolder name?: ")
-        os.makedirs(fName) #Creates a folder
+        f_Name = input("\nFolder name?: ")
+        os.makedirs(f_Name) #Creates a folder
    
     elif option == '4':
-        print(fileNoExtension()) #Prints the files without a extension
+        print(file_No_Extension()) #Prints the files without a extension
         yn = 'y'
         while yn != 'q':
-            chosenFile = input("\nWhat file (q to quit)? ")
-            if chosenFile in 'q':
+            chosen_File = input("\nWhat file (q to quit)? ")
+            if chosen_File in 'q':
                 yn = 'q'
                 continue
             try:
                 size = 0.0
-                chosenFile = fileFinder(chosenFile) #Goes find the file
-                size = os.path.getsize(chosenFile) #Gets the file size
+                chosen_File = file_Finder(chosen_File) #Goes find the file
+                size = os.path.getsize(chosen_File) #Gets the file size
             except FileNotFoundError: 
                 print("\nFile doesn't exist!")
                 continue
@@ -134,12 +137,12 @@ while True:
                 print("\nFile doesn't exist!")
                 continue
             else:
-                fileSizeFinder(size) #Pretty prints it
+                file_Size_Finder(size) #Pretty prints it
     elif option == '5':
         while True:
-            fNameAndExtension = input("\nWhat's the filename and extension you want it to be? ")
+            f_Name_And_Extension = input("\nWhat's the filename and extension you want it to be? ")
             try:
-                os.open(fNameAndExtension, os.O_RDWR|os.O_CREAT|os.O_EXCL) # os.O_RDWR, os.O_CREAT and os.O_EXCL means get readable and writeable permissions of the file
+                os.open(f_Name_And_Extension, os.O_RDWR|os.O_CREAT|os.O_EXCL) # os.O_RDWR, os.O_CREAT and os.O_EXCL means get readable and writeable permissions of the file
                                                                            #If it doesn't exist, os.O_CREAT, means create it. and EXCL is just there to make sure
                                                                            #that you don't create a file with the same name.
                 break
@@ -148,38 +151,39 @@ while True:
                 continue
                 
     elif option == '6':
-        print(fileNoExtension())
-        osFWrite = ''
+        print(file_No_Extension())
+        osF_Write = ''
         while True:
             try:
-                fName, textToWrite = input("\nTo what file? "), input("\nWhat do you want to write? ")
-                fName = fileFinder(fName) #Finds the file 
-                osFWrite = os.open(fName, os.O_RDWR) #Open it in the code with Read and Write permissions
+                f_Name = input("\nTo what file? ")
+                text_To_Write = input("\nWhat do you want to write? ")
+                f_Name = file_Finder(f_Name) #Finds the file 
+                osF_Write = os.open(f_Name, os.O_RDWR) #Open it in the code with Read and Write permissions
             except FileNotFoundError:
                 print("\nFile doesn't exist!")
                 continue
             else:
-                with open(osFWrite, 'a') as fName: #Write to the file with whatever you want, the 'a' means 
+                with open(osF_Write, 'a') as f_Name: #Write to the file with whatever you want, the 'a' means 
                                                    #append
-                    fName.write(textToWrite)
+                    f_Name.write(text_To_Write)
                     
     elif option == '7':
-        print(fileNoExtension())
+        print(file_No_Extension())
         print("\nWhat file do you want renamed?")
-        fileName = input("\r> ")
-        fileName = fileFinder(fileName) #finds the file
+        file_Name = input("\r> ")
+        file_Name = fileFinder(file_Name) #finds the file
         print("\nTo? ")
-        fileRename = input("\r> ")
+        file_Rename = input("\r> ")
         try:    
-            os.rename(fileName, fileRename) #Renames it unless it doesn't exist
+            os.rename(file_Name, file_Rename) #Renames it unless it doesn't exist
         except FileNotFoundError:
             print("\nFile doesn't exist!")
             
     elif option == '8':
-        fileDir = changeDir(fileDir) #Goes to the change dir function
+        file_Dir = change_Dir(file_Dir) #Goes to the change dir function
         
     elif option == '9':
-        print(fileDir) #Prints the current dir
+        print(file_Dir) #Prints the current dir
         
     elif option == "`": #For the more tech savvy people. 
         while True:
