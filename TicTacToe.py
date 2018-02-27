8c388c6f68fac296780999ada61bdcee6b474989
XenXenOfficial <9jaydon99@gmail.com>
2018-02-26 21:23:17 -0500

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,24 +1,23 @@
-test = [['1',' ',' ',' '],['2',' ',' ',' '],['3',' ',' ',' ']]
+import sys
+board = [[' ',' ',' '],
+        [' ',' ',' '],
+        [' ',' ',' ']]
 
 def check(x,y):
-    if test[0][y] == test[1][y] == test[2][y]:
+    if board[0][y] == board[1][y] == board[2][y]:
         return True
-    if test[x][0] == test[x][1] == test[x][2]:
+    if board[x][0] == board[x][1] == board[x][2]:
         return True
-    if x == y and test[0][0] == test[1][1] == test[2][2]:
+    if x == y and board[0][0] == board[1][1] == board[2][2]:
         return True
-    if x + y == 2 and test[0][2] == test[1][1] == test[2][0]:
+    if x + y == 2 and board[0][2] == board[1][1] == board[2][0]:
         return True
     return False
     
 def reprint():
-    print('   1', ' 2', ' 3')
-    for i in range(len(test)):
-        for i2 in range(len(test[i])):
-            print(test[i][i2], '|' ,end='')
-            
-        print()
-        
+    print('    1   2   3')
+    for i in range(len(board)):
+        print("{} | {} | {} | {} |".format(i+1, board[i][0], board[i][1], board[i][2]))
 playerTurn = True
 
 while True:
@@ -31,22 +30,24 @@ while True:
         pVal = 'Player One'
         playerTurn = False
     while True:
-        answer = input("What spot {}? ".format(pVal))
+        answer = input("\nWhat spot {}? ".format(pVal))
         answer = list(map(int,answer))
         try:
-            if test[answer[0] - 1][answer[1]] not in ('x', 'y'):
+            if board[answer[0] - 1][answer[1] - 1] not in ('x', 'y'):
                 if pVal == 'Player One':
-                    test[answer[0] - 1][answer[1]] = 'x'
-                    if check((answer[0] - 1), (answer[1])) == True:
+                    board[answer[0] - 1][answer[1] - 1] = 'x'
+                    if check((answer[0] -1), (answer[1] -1)) == True:
                         print("Player One winner!")
                         reprint()
+                        sys.exit()
                     else:
                         break
                 else:
-                    test[answer[0] - 1][answer[1]] = 'y'
-                    if check((answer[0] - 1), (answer[1])) == True:
+                    board[answer[0] - 1][answer[1] - 1] = 'y'
+                    if check((answer[0] -1), (answer[1] -1)) == True:
                         print("Player Two winner!")
                         reprint()
+                        sys.exit()
                     else:
                         break
             else:
