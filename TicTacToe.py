f2672cc3981d252038ceffdc0af2777a8d3f9e6a
XenXenOfficial <9jaydon99@gmail.com>
2018-02-26 19:11:14 -0500

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -10,12 +10,15 @@ def check(x,y):
     if x + y == 2 and test[0][2] == test[1][1] == test[2][0]:
         return True
     return False
+    
 def reprint():
     for i in range(len(test)):
         for i2 in range(len(test[i])):
             print('|',test[i][i2], '|', end='',)
         print()
+        
 playerTurn = True
+
 while True:
     pVal = ''
     reprint()
@@ -25,31 +28,27 @@ while True:
     else:
         pVal = 'Player One'
         playerTurn = False
-    if pVal == 'Player One':
-        while True:
-            answer = input("What one {}? ".format(pVal))
-            answer = list(map(int,answer))
+    while True:
+        answer = input("What spot {}? ".format(pVal))
+        answer = list(map(int,answer))
+        try:
             if test[answer[0] - 1][answer[1] - 1] not in ('x', 'y'):
-                test[answer[0] - 1][answer[1] - 1] = 'x'
-                if check((answer[0] -1), (answer[1] -1)) == True:
-                    print("Player One winner!")
-                    reprint()
+                if pVal == 'Player One':
+                    test[answer[0] - 1][answer[1] - 1] = 'x'
+                    if check((answer[0] -1), (answer[1] -1)) == True:
+                        print("Player One winner!")
+                        reprint()
+                    else:
+                        break
                 else:
-                    break
-            else:
-                print("Can't do that!")
-                continue
-    else:
-        while True:
-            answer = input("What one {}? ".format(pVal))
-            answer = list(map(int,answer))
-            if test[answer[0] - 1][answer[1] - 1] not in ('x','y'):
-                test[answer[0] - 1][answer[1] - 1] = 'y'
-                if check((answer[0] -1), (answer[1] -1)) == True:
-                    print("Player Two winner!")
-                    reprint()
-                else:
-                    break
+                    test[answer[0] - 1][answer[1] - 1] = 'y'
+                    if check((answer[0] -1), (answer[1] -1)) == True:
+                        print("Player Two winner!")
+                        reprint()
+                    else:
+                        break
             else:
                 print("Cant do that!")
-                continue
+        except IndexError:
+            print("Not a valid spot!")
+            continue
